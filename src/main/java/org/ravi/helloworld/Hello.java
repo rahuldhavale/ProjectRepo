b889d76078db1d963421723a0fb59b37f6566a64
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@accenture.com>
2018-04-06 15:52:13 +0530

    changes for quality gate

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -2,8 +2,10 @@ package org.ravi.helloworld;
 
 public class Hello {
 	
-	public static void main(String[] args) {
-		throw new UnsupportedOperationException();
+	public String displayHelloMessage(String message)
+	{
+		message = "hi";
+		return message;
 	}
 
 }
